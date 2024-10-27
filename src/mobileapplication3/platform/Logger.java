b909f284fb876f88cf92b47ed44a5e05b2dd8be3
vipoll <vipollmail@gmail.com>
2024-10-27 20:55:06 +0400

    Logger: throw IAE if trying to enable log with zero size

--- src/mobileapplication3/platform/Logger.java
+++ src/mobileapplication3/platform/Logger.java
@@ -24,6 +24,10 @@ public class Logger {
     private static boolean logToStdout = false;
 
     public static void enableOnScreenLog(int screenHeight) {
+    	System.out.println("enabling log. screen h: " + screenHeight);
+    	if (screenHeight <= 0) {
+    		throw new IllegalArgumentException("can't enable log: h=" + screenHeight);
+    	}
         isOnScreenLogEnabled = true;
         int n = screenHeight / Math.max(1, Font.getFont(Font.FACE_MONOSPACE, Font.STYLE_BOLD, Font.SIZE_SMALL).getHeight());
         if (n < 5) {
