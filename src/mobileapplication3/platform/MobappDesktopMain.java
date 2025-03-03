9cd02ea7e4b57fce1f5562cb764006dbb95f7dd4
vipoll <vipollmail@gmail.com>
2025-03-03 22:50:22 +0400

    move some code from the main class to an abstract class

---
+++ src/mobileapplication3/platform/MobappDesktopMain.java
@@ -0,0 +1,25 @@
+package mobileapplication3.platform;
+
+import mobileapplication3.platform.ui.RootContainer;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public abstract class MobappDesktopMain extends Frame {
+    public MobappDesktopMain(String[] args) {
+        setSize(1200, 900);
+        Platform.init(this);
+        RootContainer.getInst().setBgColor(0);
+        setVisible(true);
+        setLayout(new BorderLayout());
+        add(RootContainer.getInst(), BorderLayout.CENTER);
+        setMinimumSize(new Dimension(400, 300));
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
+        setLocationRelativeTo(null);
+    }
+}
