265bb09200a9347d25c691305ef66ebf650407a4
vipoll <vipollmail@gmail.com>
2026-04-18 19:10:39 +0400

    fixup! fully ignore paint(g) if root component asked repaint only on flush

--- src/mobileapplication3/platform/ui/RootContainer.java
+++ src/mobileapplication3/platform/ui/RootContainer.java
@@ -91,7 +91,7 @@ public class RootContainer extends GameCanvas implements IContainer, IPopupFeedb
                 inst.rootUIComponentPostInitDone = true;
             }
         }
-        inst.repaint();
+        inst.repaintt();
         return inst;
     }
 
@@ -106,12 +106,12 @@ public class RootContainer extends GameCanvas implements IContainer, IPopupFeedb
     }
 
     public void paint(Graphics g) {
-        if (rootUIComponent == null && bgColor >= 0) {
-            g.fillRect(0, 0, w, h);
-        }
-
         if (rootUIComponent != null && rootUIComponent.repaintOnlyOnFlushGraphics()) {
             return;
+        }
+
+        if (bgColor >= 0) {
+            g.fillRect(0, 0, w, h);
         }
 
         if (rootUIComponent != null) {
